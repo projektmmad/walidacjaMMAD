f1e46700faed03e5b6744b02a8d56b0bc63f9dba
Tysiu <56279413+Tysiu@users.noreply.github.com>
2021-02-26 01:00:47 +0100

    Add files via upload

---
+++ valid_functions_MT.py
@@ -0,0 +1,98 @@
+from lxml import etree
+from methods import *
+import xml.etree.ElementTree as ET
+
+'''read_xml is a function to read a xml file'''
+def read_xml():
+    global path, tree, root
+    #path = input("Podaj ścieżkę: ")
+    path = '../data_examples/VOC2012/Annotations/2007_000027.xml'
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+read_xml()
+
+''' 
+syntax_validator(path) is a function to validate syntax error. It works how it goes: we open xml file as txt file,
+then transform it from string to the tree. If there is something wrong with the syntax, we can't transform it. 
+'''
+
+def syntax_validator(path):
+    with open(path, 'r') as file:
+        txt_xml_file = file.read().replace('','')
+    try:
+        etree.fromstring(txt_xml_file)
+    except BaseException as e:
+        print(e)
+
+'''
+check_extension is a function to check if file extension in attribute 'filename' is correct
+'''
+
+def check_extension(path):
+    str_jpg = root.find('filename').text
+    if str_jpg.endswith('.jpg') != True:
+        print('Invalid file extension')
+        return 1
+    return 0
+
+'''
+bnd_validator is a function to validate if bndbox  has a right values of coordinates xmax, xmin, ymax, ymin.  
+'''
+
+def bndbox_validator(path):
+    list_xmax = []
+    list_xmin = []
+    list_ymax = []
+    list_ymin = []
+
+    for branch in root.iter('bndbox'):
+        list_xmax += [int(leaf.text) for leaf in branch if leaf.tag == 'xmax']
+        list_xmin += [int(leaf.text) for leaf in branch if leaf.tag == 'xmin']
+        list_ymax += [int(leaf.text) for leaf in branch if leaf.tag == 'ymax']
+        list_ymin += [int(leaf.text) for leaf in branch if leaf.tag == 'ymin']
+
+    size = root.find("size")
+    width = int(size.find("width").text)
+    height = int(size.find("height").text)
+
+    if len(list_xmax) == len(list_xmin) and len(list_ymax) == len(list_ymin) and len(list_xmax) == len(list_ymax):
+        if diff_if_negative(list_xmax, list_xmin) == True or diff_if_negative(list_ymax, list_ymin) == True:
+            print('Difference between coordinates xmax and xmin or ymax and ymin is negative: '
+                  'fragmentation beyond the graph')
+            return 1
+        for (i, j, k, l) in zip(list_xmax, list_xmin, list_ymax, list_ymin):
+            if i <= width and i > 0 and j < width and j >= 0 and k <= height and k > 0 and l < height and l >= 0:
+                continue
+            else:
+                print('Coordinates xmax, xmin, ymax, ymin are wrong with height and width value:'
+                      ' fragmentation beyond the graph ')
+                return 1
+    else:
+        print('Wrong amount of coordinates xmax, xmin, ymax, ymin')
+        return 1
+    return 0
+'''
+dtd_validator is a function to validate if 'depth', 'truncated' and 'difficult' values are right.  
+'''
+
+def dtd_validator(path):
+    for n in root.iter('depth'):
+        if int(n.text) != 3 and int(n.text) != 0:
+            print(" invalid value of 'depth' ")
+            return 1
+
+    for n in root.iter('truncated'):
+        if int(n.text) != 1 and int(n.text) != 0:
+            print(" invalid value of 'truncated' ")
+            return 1
+
+    for n in root.iter('difficult'):
+        if int(n.text) != 1 and int(n.text) != 0:
+            print(" invalid value of 'difficult' ")
+            return 1
+    return 0
+
+syntax_validator(path)
+bndbox_validator(path)
+check_extension(path)
