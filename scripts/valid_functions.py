6ec24778f6ffb9847c7a850438db4f139b482e78
stelmachowicz <sandrastelmach3@gmail.com>
2021-03-09 21:16:30 +0100

    Change bnd_validator

--- scripts/valid_functions.py
+++ scripts/valid_functions.py
@@ -33,36 +33,43 @@ def check_extension(path):
 bnd_validator is a function to validate if bndbox  has a right values of coordinates xmax, xmin, ymax, ymin.  
 '''
 
-def bndbox_validator(path):
-    tree = ET.parse(path)
-    root = tree.getroot()
-    list_xmax = []
-    list_xmin = []
-    list_ymax = []
-    list_ymin = []
+tree = ET.parse(path)
+root = tree.getroot()
+list_xmax = []
+list_xmin = []
+list_ymax = []
+list_ymin = []
 
-    for branch in root.iter('bndbox'):
-        list_xmax += [int(leaf.text) for leaf in branch if leaf.tag == 'xmax']
-        list_xmin += [int(leaf.text) for leaf in branch if leaf.tag == 'xmin']
-        list_ymax += [int(leaf.text) for leaf in branch if leaf.tag == 'ymax']
-        list_ymin += [int(leaf.text) for leaf in branch if leaf.tag == 'ymin']
 
-    size = root.find("size")
-    width = int(size.find("width").text)
-    height = int(size.find("height").text)
+for branch in root.iter('bndbox'):
+    list_xmax += [int(leaf.text) for leaf in branch if leaf.tag == 'xmax']
+    list_xmin += [int(leaf.text) for leaf in branch if leaf.tag == 'xmin']
+    list_ymax += [int(leaf.text) for leaf in branch if leaf.tag == 'ymax']
+    list_ymin += [int(leaf.text) for leaf in branch if leaf.tag == 'ymin']
 
-    if len(list_xmax) == len(list_xmin) and len(list_ymax) == len(list_ymin) and len(list_xmax) == len(list_ymax):
-        if diff_if_negative(list_xmax, list_xmin) == True or diff_if_negative(list_ymax, list_ymin) == True:
-            print('Difference between coordinates xmax and xmin or ymax and ymin is negative: '
-                  'fragmentation beyond the graph')
-        for (i, j, k, l) in zip(list_xmax, list_xmin, list_ymax, list_ymin):
-            if i <= width and i > 0 and j < width and j >= 0 and k <= height and k > 0 and l < height and l >= 0:
-                continue
-            else:
-                print('Coordinates xmax, xmin, ymax, ymin are wrong with height and width value:'
-                      ' fragmentation beyond the graph ')
-    else:
+
+size = root.find("size")
+width = int(size.find("width").text)
+height = int(size.find("height").text)
+
+
+
+
+def bnd_validator1(path):
+    if len(list_xmax) != len(list_xmin) or len(list_ymax) != len(list_ymin) or len(list_xmax) != len(list_ymax):
         print('Wrong amount of coordinates xmax, xmin, ymax, ymin')
+
+def bnd_validator2(path):
+    if diff_if_negative(list_xmax, list_xmin) == True or diff_if_negative(list_ymax, list_ymin) == True:
+        print('Difference between coordinates xmax and xmin or ymax and ymin is negative: '
+              'fragmentation beyond the graph ndaisodja')
+def bnd_validator3(path):
+    for (i, j, k, l) in zip(list_xmax, list_xmin, list_ymax, list_ymin):
+        if i <= width and i > 0 and j < width and j >= 0 and k <= height and k > 0 and l < height and l >= 0:
+            continue
+        else:
+            print('Coordinates xmax, xmin, ymax, ymin are wrong with height and width value:'
+                  ' fragmentation beyond the graph ')
 
 '''
 dtd_validator is a function to validate if 'depth', 'truncated' and 'difficult' values are right.  
@@ -84,5 +91,4 @@ def dtd_validator(path):
 
 
 syntax_validator(path)
-bndbox_validator(path)
 
