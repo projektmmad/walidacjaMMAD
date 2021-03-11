e0baf3ed7d1b36f0352666f1dbf303dc69cb56c1
Tysiu <matitys@gmail.com>
2021-03-11 17:36:56 +0100

    Wstępny zarys funkcji robiącej listę wszystkich pkiów w folderze

--- test/valid_functions_MT.py
+++ test/valid_functions_MT.py
@@ -1,12 +1,19 @@
 from lxml import etree
 from methods import *
 import xml.etree.ElementTree as ET
+from os import listdir
 
 '''read_xml is a function to read a xml file'''
 
-def read_xml():
-    #path = input("Podaj ścieżkę: ")
-    path = '../data_examples/VOC2012/Annotations/2007_000027.xml'
+def files_list(path):
+    list = listdir(path)
+    for i in range(len(list)):
+        list[i] = path + '/' + list[i]
+    return list
+
+
+def read_xml(path):
+    path = path
     tree = ET.parse(path)
     root = tree.getroot()
     str_jpg = root.find('filename').text
@@ -58,35 +65,55 @@ def check_extension(extension):
 bnd_validator is a function to validate if bndbox  has a right values of coordinates xmax, xmin, ymax, ymin.  
 '''
 
-def bndbox_validator(size, width, length):
-    list_xmax = []
-    list_xmin = []
-    list_ymax = []
-    list_ymin = []
+'''
+bnd_validator is a function to validate if bndbox  has a right values of coordinates xmax, xmin, ymax, ymin.  
+'''
 
-    for branch in root.iter('bndbox'):
-        list_xmax += [int(leaf.text) for leaf in branch if leaf.tag == 'xmax']
-        list_xmin += [int(leaf.text) for leaf in branch if leaf.tag == 'xmin']
-        list_ymax += [int(leaf.text) for leaf in branch if leaf.tag == 'ymax']
-        list_ymin += [int(leaf.text) for leaf in branch if leaf.tag == 'ymin']
+'''
+def empty_min_max(path):
+    for k in ['xmax', 'xmin','ymax','ymin']:
+        for m in root.iter(k):
+            if m.text == None:
+                print('Empty value in',k )
 
-    if len(list_xmax) == len(list_xmin) and len(list_ymax) == len(list_ymin) and len(list_xmax) == len(list_ymax):
-        if diff_if_negative(list_xmax, list_xmin) == True or diff_if_negative(list_ymax, list_ymin) == True:
-            print('Difference between coordinates xmax and xmin or ymax and ymin is negative: '
-                  'fragmentation beyond the graph')
-            return False
-        for (i, j, k, l) in zip(list_xmax, list_xmin, list_ymax, list_ymin):
-            if 0 < i <= width and 0 <= j < width and 0 < k <= height and 0 <= l < height:
-                continue
-            else:
-                print('Coordinates xmax, xmin, ymax, ymin are wrong with height and width value:'
-                      ' fragmentation beyond the graph ')
-                return False
-    else:
+tree = ET.parse(path)
+root = tree.getroot()
+list_xmax = []
+list_xmin = []
+list_ymax = []
+list_ymin = []
+
+for branch in root.iter('bndbox'):
+    list_xmax += [int(leaf.text) for leaf in branch if leaf.tag == 'xmax']
+    list_xmin += [int(leaf.text) for leaf in branch if leaf.tag == 'xmin']
+    list_ymax += [int(leaf.text) for leaf in branch if leaf.tag == 'ymax']
+    list_ymin += [int(leaf.text) for leaf in branch if leaf.tag == 'ymin']
+
+
+size = root.find("size")
+width = int(size.find("width").text)
+height = int(size.find("height").text)
+
+
+
+
+def bnd_validator1(path):
+    if len(list_xmax) != len(list_xmin) or len(list_ymax) != len(list_ymin) or len(list_xmax) != len(list_ymax):
         print('Wrong amount of coordinates xmax, xmin, ymax, ymin')
-        return False
-    return True
 
+def bnd_validator2(path):
+    if diff_if_negative(list_xmax, list_xmin) == True or diff_if_negative(list_ymax, list_ymin) == True:
+        print('Difference between coordinates xmax and xmin or ymax and ymin is negative: '
+              'fragmentation beyond the graph ndaisodja')
+def bnd_validator3(path):
+    for (i, j, k, l) in zip(list_xmax, list_xmin, list_ymax, list_ymin):
+        if i <= width and i > 0 and j < width and j >= 0 and k <= height and k > 0 and l < height and l >= 0:
+            continue
+        else:
+            print('Coordinates xmax, xmin, ymax, ymin are wrong with height and width value:'
+                  ' fragmentation beyond the graph ')
+
+'''
 
 '''Values validator checks if a value of checked atribute is valid or not. It's a poprawioned version of dtd_validator'''
 
@@ -99,14 +126,18 @@ def values_validator(root, checked_atribute, valid_values):
             print("Invalid value of " + checked_atribute)
             return False
 
-def execute():
-    path, tree, root, str_jpg, database, d_annotation, size, width, height = read_xml()
+lista = files_list('../data_examples/VOC2012/Annotations')
+
+def execute(path):
+    read_xml(path)
+    path, tree, root, str_jpg, database, d_annotation, size, width, height = read_xml(path)
     syntax_validator(path)
-    bndbox_validator(size, width, height)
+    #bndbox_validator(root, size, width, height)
     check_extension(str_jpg)
     check_database(database, d_annotation)
     values_validator(root, 'depth', {0, 3})
     values_validator(root, 'truncated', {0, 1})
     values_validator(root, 'difficult', {0, 1})
 
-execute()
+for i in range(len(lista)):
+    execute(lista[i])
