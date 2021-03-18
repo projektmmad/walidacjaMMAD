9accf167830a29dea9a89dbff12912ddd0e3eeec
Anna-Wasik <ania.wasik.19@gmail.com>
2021-03-18 22:37:22 +0100

    Update

--- scripts/pascal_validator.py
+++ scripts/pascal_validator.py
@@ -1,11 +1,13 @@
 from validation_merge import *
+from VOC2012_dir_structure import VOC2012_structure
 
-path = '../data_examples/VOC2012/Annotations/2007_000027.xml'
+path_to_VOC = '/home/annaw/VOCdevkit/VOC2012'
 
-def pascal_validator(path):
-    dir_annotations = os.path.dirname(path)
-    list_file = os.listdir(dir_annotations)
-    for file in list_file:
-        pascal_validator_one_file(f'{dir_annotations}/{file}')
+def pascal_validator(path_to_VOC):
+    if VOC2012_structure(path_to_VOC) is not False:
+        dir_annotations = path_to_VOC+'/Annotations'
+        list_file = os.listdir(dir_annotations)
+        for file in list_file:
+            pascal_validator_one_file(f'{dir_annotations}/{file}')
 
-pascal_validator(path)
+pascal_validator(path_to_VOC)
