aaaf00443deac36624930f423fe7d3924694406b
stelmachowicz <sandrastelmach3@gmail.com>
2021-03-25 21:53:41 +0100

    add VOC2012_trash function and update VOC2012_structure

--- scripts/VOC2012_dir_structure.py
+++ scripts/VOC2012_dir_structure.py
@@ -3,15 +3,24 @@ from methods import *
 def check_folder_structure(path_to_VOC2012, dirname):
     if os.path.isdir(f'{path_to_VOC2012}/{dirname}') is False:
         print(f'Folder structure error: there is no {dirname} directory')
+    else:
+        return True
+
+def VOC2012_trash(path_to_VOC):
+    dir_name = ['Annotations', 'ImageSets', 'JPEGImages', 'SegmentationClass', 'SegmentationObject', '.git']
+    dir_VOC = os.listdir(path_to_VOC)
+    for katalog in dir_VOC:
+        if not katalog in dir_name:
+            print(f'Warning: additional directory or wrong dirname in VOC folder {katalog}')
 
 
 def VOC2012_structure(path_to_VOC2012):
+    VOC2012_trash(path_to_VOC2012)
+    dir_name = ['Annotations', 'ImageSets', 'JPEGImages', 'SegmentationClass', 'SegmentationObject']
     if os.path.isdir(path_to_VOC2012) is True:
-        check_folder_structure(path_to_VOC2012, 'Annotations')
-        check_folder_structure(path_to_VOC2012, 'ImageSets')
-        check_folder_structure(path_to_VOC2012, 'JPEGImages')
-        check_folder_structure(path_to_VOC2012, 'SegmentationClass')
-        check_folder_structure(path_to_VOC2012, 'SegmentationObject')
+        for name in dir_name:
+            check_folder_structure(path_to_VOC2012, name)
     else:
         print('Wrong path to VOC2012 folder')
         return False
+
