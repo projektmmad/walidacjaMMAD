47de4cc88d566f606e9a6841923496662d066efb
stelmachowicz <sandrastelmach3@gmail.com>
2021-04-08 22:55:15 +0200

    update

--- scripts/VOC2012_dir_structure.py
+++ scripts/VOC2012_dir_structure.py
@@ -1,33 +1,82 @@
-from methods import *
+from validation_merge import *
 
-def check_folder_structure(path_to_VOC2012, dirname):
-    if os.path.isdir(f'{path_to_VOC2012}/{dirname}') is False:
-        print(f'Folder structure error: there is no {dirname} directory')
-    else:
+def check_path(path_to_VOC):
+    if os.path.isdir(path_to_VOC) is True:
         return True
-
-def VOC2012_structure(path_to_VOC2012):
-    dir_name = ['Annotations', 'ImageSets', 'JPEGImages', 'SegmentationClass', 'SegmentationObject']
-    if os.path.isdir(path_to_VOC2012) is True:
-        for name in dir_name:
-            check_folder_structure(path_to_VOC2012, name)
     else:
-        print('Wrong path to VOC2012 folder')
         return False
 
+def check_ann(path_to_VOC):
+    if os.path.isdir(path_to_VOC + '/Annotations') is True:
+        return True
+    else:
+        return False
 
-# VOC2012_trash(path_to_VOC) - the function checks if there are any unnecessary files in the VOC folder
-def VOC2012_trash(path_to_VOC):
+def check_jpg(path_to_VOC):
+    if os.path.isdir(path_to_VOC + '/JPEGImages') is True:
+        return True
+    else:
+        return False
+
+def check_structure(path_to_VOC):
+    if check_path(path_to_VOC) is True:
+        if check_ann(path_to_VOC) is False or check_jpg(path_to_VOC) is False:
+            print("Folder structure error: there is no \"Annotations\" or \"JPEGImages\" directory")
+            return False
+    else:
+        print('Wrong path to VOC folder')
+        return False
+
+def VOC_trash(path_to_VOC):
     dir_name = ['Annotations', 'ImageSets', 'JPEGImages', 'SegmentationClass', 'SegmentationObject', '.git']
     dir_VOC = os.listdir(path_to_VOC)
     for katalog in dir_VOC:
         if not katalog in dir_name:
             print(f'Warning: additional directory or wrong dirname in VOC folder {katalog}')
 
+def VOC_warning_dir(path_to_VOC):
+    dir_name = ['ImageSets', 'SegmentationClass', 'SegmentationObject']
+    dir_VOC = os.listdir(path_to_VOC)
+    for katalog in dir_name:
+        if not katalog in dir_VOC:
+            print(f'Warning: Missing folder: \"{katalog}\" ')
+
+def trash_in_ann(path_to_VOC):
+    for file_xml in os.listdir(path_to_VOC + '/Annotations'):
+        if file_xml.endswith('.xml') is False:
+            print(f"Error: Invalid file(s) in folder 'Annotations' - there should be only .xml files ")
+            return False
+
+def trash_in_jpg(path_to_VOC):
+    for file_jpg in os.listdir(path_to_VOC + '/JPEGImages'):
+        if file_jpg.endswith('.jpg') is False and file_jpg.endswith('.jpeg') is False:
+            print(f"Error: Invalid file in folder 'JPEGImages' - there should be only .jp(e)g files ")
+            return False
+
+# jpg_xml(path_to_VOC) - the function checks if the files are the same in two folders: '/Annotations' and /JPEGImages'
+def jpg_xml(path_to_VOC):
+    lista_jpg = []
+    lista_xml = []
+    for file_xml in os.listdir(path_to_VOC + '/Annotations'):
+        file_xml_split = file_xml.split(".")
+        lista_xml.append(file_xml_split[0])
+    for file_jpg in os.listdir(path_to_VOC + '/JPEGImages'):
+        file_jpg_split = file_jpg.split(".")
+        lista_jpg.append(file_jpg_split[0])
+    help_xml = []
+    for a in lista_xml:
+        if a in lista_jpg:
+            lista_jpg.remove(a)
+        else:
+            help_xml.append(a)
+    if lista_jpg != []:
+        print(f"The following name-files: {lista_jpg} in the folder 'JPEGImages' have no match in the folder '/Annotations' ")
+    if help_xml != []:
+        print(f"The following name-files: {help_xml} in the folder '/Annotations' have no match in the folder 'JPEGImages' ")
+
 
 # VOC2012_trash(path_to_VOC) - the function checks if there are only xml files in the Annotation folder,
 # jpg files in the JPEGImage and png files in SegmentationClass, SegmentationObject
-
 def trash_folders(path_to_VOC):
     for file_xml in os.listdir(path_to_VOC + '/Annotations'):
         if file_xml.endswith('.xml') is not True:
@@ -41,34 +90,3 @@ def trash_folders(path_to_VOC):
     for file_png in os.listdir(path_to_VOC + '/SegmentationObject'):
         if file_png.endswith('.png') is not True:
             print(f"Invalid file '{file_png}' in folder 'SegmentationObject' ")
-
-
-# jpg_xml(path_to_VOC) - the function checks if the files are the same in two folders: '/Annotations' and /JPEGImages'
-
-def jpg_xml(path_to_VOC):
-    lista_jpg = []
-    lista_xml = []
-    for file_xml in os.listdir(path_to_VOC + '/Annotations'):
-        file_xml_split = file_xml.split(".")
-        lista_xml.append(file_xml_split[0])
-    print(lista_xml)
-    for file_jpg in os.listdir(path_to_VOC + '/JPEGImages'):
-        file_jpg_split = file_jpg.split(".")
-        lista_jpg.append(file_jpg_split[0])
-    print(lista_jpg)
-    help_xml=[]
-    for a in lista_xml:
-        if a in lista_jpg:
-            lista_jpg.remove(a)
-        else:
-            help_xml.append(a)
-    if lista_jpg != []:
-        print(f"The following name-files: {lista_jpg} in the folder 'JPEGImages' have no match in the folder '/Annotations' ")
-    if help_xml != []:
-        print(f"The following name-files: {help_xml} in the folder '/Annotations' have no match in the folder 'JPEGImages' ")
-
-
-#path_to_VOC = '/home/sandra/walidacjaMMAD/data_examples/VOC2012'
-#jpg_xml(path_to_VOC)
-
-
--- scripts/pascal_validator.py
+++ scripts/pascal_validator.py
@@ -1,16 +1,17 @@
-from validation_merge import *
-from VOC2012_dir_structure import VOC2012_structure
-
-#path_to_VOC = '/home/annaw/VOCdevkit/VOC2012'
-path_to_VOC = '/home/sandra/walidacjaMMAD/data_examples/VOC2012'
+from VOC2012_dir_structure import *
 
 def pascal_validator(path_to_VOC):
-    if VOC2012_structure(path_to_VOC) is not None:
-        try:
-            dir_annotations = path_to_VOC+'/Annotations'
-        except:
+    if check_structure(path_to_VOC) is not False:
+        VOC_trash(path_to_VOC)
+        VOC_warning_dir(path_to_VOC)
+        if trash_in_ann(path_to_VOC) is not False and trash_in_jpg(path_to_VOC) is not False:
+            jpg_xml(path_to_VOC)
+            dir_annotations = path_to_VOC + '/Annotations'
             list_file = os.listdir(dir_annotations)
             for file in list_file:
                 pascal_validator_one_file(f'{dir_annotations}/{file}')
 
+
+#path_to_VOC = '/home/annaw/VOCdevkit/VOC2012'
+path_to_VOC = '/home/sandra/walidacjaMMAD/data_examples/VOC2012'
 pascal_validator(path_to_VOC)
