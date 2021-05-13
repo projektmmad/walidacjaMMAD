f8f860bc93c2313844e5a2b6165a2102788e903b
Anna-Wasik <ania.wasik.19@gmail.com>
2021-05-13 20:29:26 +0200

    add if on the beginning of pascal_validator.py

--- scripts/pascal_validator.py
+++ scripts/pascal_validator.py
@@ -4,15 +4,15 @@ import argparse
 
 
 def pascal_validator(path_to_voc: str):
-    check_folder_structure(path_to_voc)
-    voc_warning_add_dir(path_to_voc)
-    voc_warning_miss_dir(path_to_voc)
-    if trash_in_ann(path_to_voc) is not False and trash_in_jpg(path_to_voc) is not False:
-        jpg_xml(path_to_voc)
-        dir_annotations = path_to_voc + '/Annotations'
-        list_file = os.listdir(dir_annotations)
-        for file in list_file:
-            pascal_validator_one_file(f'{dir_annotations}/{file}')
+   if check_folder_structure(path_to_voc) is not False:
+        voc_warning_add_dir(path_to_voc)
+        voc_warning_miss_dir(path_to_voc)
+        if trash_in_ann(path_to_voc) is not False and trash_in_jpg(path_to_voc) is not False:
+            jpg_xml(path_to_voc)
+            dir_annotations = path_to_voc + '/Annotations'
+            list_file = os.listdir(dir_annotations)
+            for file in list_file:
+                pascal_validator_one_file(f'{dir_annotations}/{file}')
 
 
 parser = argparse.ArgumentParser()
