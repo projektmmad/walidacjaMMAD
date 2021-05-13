8beb52343a6ffd994fd120fbdfdfdaf78b2d5ae4
akostkiewicz <56300126+akostkiewicz@users.noreply.github.com>
2021-05-13 11:14:11 +0200

    Update unique_id.py
    Dodanie funkcji lista_id, która osobno zwraca list_id, poprawienie id_unique i wywołanie tej funkcji na sekcjach za pomocą pętli

--- unique_id.py
+++ unique_id.py
@@ -1,20 +1,27 @@
 import json
+import numpy as np
 with open("C:/Users/akost/PycharmProjects/Projekt_walidacja_intel/data.json", 'r') as f:
     data = f.read()
 python_obj = json.loads(data)
 
-def id_unique(a):
+def lista_id(a):
     list_id = []
     for i in python_obj[a]:
         list_id.append(i['id'])
+    return list_id
 
+def id_unique(a):
+    list_id = lista_id(a)
     set_id = set()
     for i in python_obj[a]:
         set_id.add(i['id'])
 
     if len(list_id) == len(set_id):
-        print("id jest unikalne")
+        return True
     else:
-        print("id nie jest unikalne")
+        return False
 
-id_unique("images")
+lista_sekcje=["images","licenses","annotations","categories"]
+for i in lista_sekcje:
+    id_unique(i)
+print(id_unique(i))
