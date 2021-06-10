da9c1ed1ad16576988ab47eb51b1815266cfaaa4
BeataMilczynska <61426724+BeataMilczynska@users.noreply.github.com>
2021-06-10 22:28:53 +0200

    aktualna

--- scripts/coco_syntax_json_validator.py
+++ scripts/coco_syntax_json_validator.py
@@ -1,16 +1,39 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import json
-#if name usunąć by można było importować skrypt
-if __name__ == "__main__":
+{ "$schema": "http://json-schema.org/draft-07/schema#" }
 
-    def syntax_validator(path, filename):
+        def syntax_validator(path, filename):
+            try:
+                with open("data.json") as json_file:
+                    data = json.load(file)
+                    print("Plik ma poprawny format")
+            except ValueError as e:
+                print("Plik jest złego formatu")
+                return False
+
+            if not isinstance(data, dict):
+                raise ParseError("Expected an object!")
+
+             try:
+            name = data['data.json']
+            except KeyError:
+                raise ParseError('Expected a name')
+
+            if not isinstance(name, dict):
+                raise ParseError("Expected an object for name")
+
+            try:
+             first = name['data.json']
+            except KeyError:
+                 raise ParseError("Expected a first name")
+
+            if not isinstance(first, basestring):
+                raise ParseError("Expected first name to be a string")
+
+             if first == '':
+                raise ParseError("Expected non-empty first name")
+
+if __name__ == "__main__":
+    with open("data.json") as file:
         try:
-            with open("data.json") as json_file:
-                data = json.load(file)
-                print("Plik ma poprawny format")
-        except Exception as e:
-            #trzeba zawezic exception, by było dokładniejsze
-            #json load jakie zwraca wyjatki
-            print("Plik jest złego formatu")
-            return False
+            data=json.load(file)
+            except
